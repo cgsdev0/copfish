a74b9de57b60aa62f4ba5b4653a33c4a389b687d
Sarah Schulte <git@sarah.engineer>
2025-04-28 12:57:47 -0700

    cop fish updates

--- start.sh
+++ start.sh
@@ -34,37 +34,10 @@ mkdir -p uploads
 
 touch "$FISH_ROOT/license"
 
-if [[ -z "$(cat "$FISH_ROOT/license")" ]]; then
-  source ./fishutils.sh
-  LICENSE="$(gen_license)"
-  echo "${LICENSE,,}" > "$FISH_ROOT/license"
-  tbus_send "new-license" "{\"license\": \"$LICENSE\"}"
-fi
-
-
 PORT=${PORT:-3000}
 
-TCP_PROVIDER=${TCP_PROVIDER:-tcpserver}
-
-case "$TCP_PROVIDER" in
-  tcpserver)
-    echo -n "Listening on port "
-    tcpserver -1 -o -l 0 -H -R -c 1000 0 $PORT ./core.sh
-    ;;
-  nc)
-    [[ ! -p nc_tunnel ]] && mkfifo nc_tunnel
-    [[ "${DEV:-true}" == true ]] && \
-      echo "WARNING: performance while using netcat will be significantly degraded!"
-    echo "Listening on port $PORT"
-    while true; do
-      < nc_tunnel nc -l $PORT | ./core.sh >nc_tunnel
-    done
-    ;;
-  *)
-    echo "ERROR: unsupported TCP_PROVIDER"
-    exit 1
-    ;;
-esac
+echo -n "Listening on port "
+tcpserver -1 -o -l 0 -H -R -c 1000 0 $PORT ./core.sh
 
 if [[ ! -z "$PID" ]]; then
   kill "$PID"
